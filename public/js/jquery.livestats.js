9a3fab555d0b0b8cd0bf7d1e9aaf23c64dc22df2
Khurrum M <khurrum1@gmail.com>
2011-08-07 09:32:15 -0400

    final

---
+++ public/js/jquery.livestats.js
@@ -0,0 +1,28 @@
+function LiveStatsClient() {
+  if (! (this instanceof arguments.callee)) {
+    return new arguments.callee(arguments);
+  }
+  
+  var self = this;
+
+  this.init = function() {
+    self.setupBayeuxHandlers();
+  };
+
+  this.setupBayeuxHandlers = function() {
+    self.client = new Faye.Client('http://localhost:8000/faye', {
+	  timeout: 120
+	});
+
+    self.client.subscribe('/stat', function(message) {
+	  console.log('MESSAGE', message);
+	}); 
+  };
+  this.init();
+};
+
+var liveStatsClient;
+
+jQuery(function() {
+  liveStatsClient = new LiveStatsClient();
+});
